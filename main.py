08d300bcac44546d14f788ad34ad815809633fb2
monk3yd <jegajardog@gmail.com>
2022-02-18 02:40:56 -0300

    OOP Aproach. Change Text by Button Press

--- main.py
+++ main.py
@@ -3,31 +3,39 @@ from PyQt6.QtWidgets import QApplication, QMainWindow
 import sys
 
 
-def click():
-    print("Click")
+class MyWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        # Position & Dimensions
+        self.setGeometry(200, 200, 300, 300)  # xpos, ypos, width, height
+        # Title
+        self.setWindowTitle("This is Mine - Watermarking App")
+        self.initUI()
+
+    def initUI(self):
+        # Label
+        self.label = QtWidgets.QLabel(self)  # Input label location
+        self.label.setText("My First Label")  # Label Text
+        self.label.move(50, 50)
+
+        # Button
+        self.button = QtWidgets.QPushButton(self)
+        self.button.setText("Click Me!")
+        # Link button to click func
+        self.button.clicked.connect(self.click)
+
+    def click(self):
+        self.label.setText("You pressed the button!")
+        self.update()
+
+    def update(self):
+        self.label.adjustSize()
 
 
 # Setup Application
 def window():
     app = QApplication(sys.argv)
-    win = QMainWindow()
-
-    # Position & Dimensions
-    win.setGeometry(200, 200, 300, 300)  # xpos, ypos, width, height
-
-    # Title
-    win.setWindowTitle("This is Mine - Watermarking App")
-
-    # Label
-    label = QtWidgets.QLabel(win)  # Input label location
-    label.setText("My First Label")  # Label Text
-    label.move(50, 50)
-
-    # Button
-    button = QtWidgets.QPushButton(win)
-    button.setText("Click Me!")
-    # Link button to click func
-    button.clicked.connect(click)
+    win = MyWindow()
 
     # Show GUI
     win.show()
