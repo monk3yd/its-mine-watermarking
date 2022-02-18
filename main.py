51f62ada5d6dd9c00d1ac283d26ce5de8134c7fd
monk3yd <jegajardog@gmail.com>
2022-02-18 02:25:44 -0300

    Before OOP

--- main.py
+++ main.py
@@ -3,18 +3,31 @@ from PyQt6.QtWidgets import QApplication, QMainWindow
 import sys
 
 
+def click():
+    print("Click")
+
+
 # Setup Application
 def window():
     app = QApplication(sys.argv)
     win = QMainWindow()
+
     # Position & Dimensions
     win.setGeometry(200, 200, 300, 300)  # xpos, ypos, width, height
+
     # Title
     win.setWindowTitle("This is Mine - Watermarking App")
+
     # Label
     label = QtWidgets.QLabel(win)  # Input label location
     label.setText("My First Label")  # Label Text
     label.move(50, 50)
+
+    # Button
+    button = QtWidgets.QPushButton(win)
+    button.setText("Click Me!")
+    # Link button to click func
+    button.clicked.connect(click)
 
     # Show GUI
     win.show()
